6a7f8ffe451271a622c6f367dd2cf55427f08816
MattSeligman <matt@modelitup.com>
2022-03-06 00:50:18 -0800

    Starting point for UI Controls

---
+++ menu.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  let menuBtn = document.getElementById("menu");
+  
+  menuBtn.onclick = function() { toggleMenu()};
+  
+ }
+ 
+
+
+function toggleMenu(){
+  console.log("Clicked");
+}
