02acd6e436fa9dac3e6c78f4f8400e05e68ca0c8
MattSeligman <matt@modelitup.com>
2022-03-06 22:56:10 -0800

    Added toggleMenu allowing add/removal of class to hide/appear levelOne menu

--- menu.js
+++ menu.js
@@ -1,12 +1,27 @@
 window.onload = function () {
   let menuBtn = document.getElementById("menu");
-  
+  let backBtn = document.getElementById("backBtn");
+
   menuBtn.onclick = function() { toggleMenu()};
-  
+  backBtn.onclick = function() { toggleMenu()};
+    
  }
  
+ /* Section below to be continued */
+function toggleHover(){
+  let itemHovered = document.getElementById("levelOne");
 
+  console.log(itemHovered);
+}
+ /* Section above to be continued */
 
 function toggleMenu(){
-  console.log("Clicked");
+  
+  let levelOne = document.getElementById("levelOne");
+  
+  if (levelOne.classList.contains("hidden")){
+    levelOne.classList.remove("hidden");
+  } else {
+    levelOne.classList.add("hidden");
+  }
 }
